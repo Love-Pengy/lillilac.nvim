eb8989996e547bd159635b1c3235f98659aad0d4
Love-Pengy <afrazierboy2001@gmail.com>
2024-06-05 13:19:05 -0400

    too lazy to source

--- colors/lillilac.lua
+++ colors/lillilac.lua
@@ -47,6 +47,51 @@ local colors = {
 	["@tag"] = { fg = "#A39EFA" },
 	["@type.definition"] = { fg = "#A39EFA" },
 	["@variable"] = { fg = "#FDFDF7" },
+	-- sym"@text.literal"      { }, -- Comment
+	-- sym"@text.reference"    { }, -- Identifier
+	-- sym"@text.title"        { }, -- Title
+	-- sym"@text.uri"          { }, -- Underlined
+	-- sym"@text.underline"    { }, -- Underlined
+	-- sym"@text.todo"         { }, -- Todo
+	-- sym"@comment"           { }, -- Comment
+	-- sym"@punctuation"       { }, -- Delimiter
+	-- sym"@constant"          { }, -- Constant
+	-- sym"@constant.builtin"  { }, -- Special
+	-- sym"@constant.macro"    { }, -- Define
+	-- sym"@define"            { }, -- Define
+	-- sym"@macro"             { }, -- Macro
+	-- sym"@string"            { }, -- String
+	-- sym"@string.escape"     { }, -- SpecialChar
+	-- sym"@string.special"    { }, -- SpecialChar
+	-- sym"@character"         { }, -- Character
+	-- sym"@character.special" { }, -- SpecialChar
+	-- sym"@number"            { }, -- Number
+	-- sym"@boolean"           { }, -- Boolean
+	-- sym"@float"             { }, -- Float
+	-- sym"@function"          { }, -- Function
+	-- sym"@function.builtin"  { }, -- Special
+	-- sym"@function.macro"    { }, -- Macro
+	-- sym"@parameter"         { }, -- Identifier
+	-- sym"@method"            { }, -- Function
+	-- sym"@field"             { }, -- Identifier
+	-- sym"@property"          { }, -- Identifier
+	-- sym"@constructor"       { }, -- Special
+	-- sym"@conditional"       { }, -- Conditional
+	-- sym"@repeat"            { }, -- Repeat
+	-- sym"@label"             { }, -- Label
+	-- sym"@operator"          { }, -- Operator
+	-- sym"@keyword"           { }, -- Keyword
+	-- sym"@exception"         { }, -- Exception
+	-- sym"@variable"          { }, -- Identifier
+	sym("@type")({ fg = "#A39EFA" }), -- Type
+	sym("@type.definition")({ fg = "#A39EFA" }), -- Typedef
+	-- sym"@storageclass"      { }, -- StorageClass
+	-- sym"@structure"         { }, -- Structure
+	-- sym"@namespace"         { }, -- Identifier
+	-- sym"@include"           { }, -- Include
+	-- sym"@preproc"           { }, -- PreProc
+	-- sym"@debug"             { }, -- Debug
+	-- sym"@tag"               { }, -- Tag
 	-- PATCH_CLOSE
 	-- content here will not be touched
 }
